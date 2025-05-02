43277418ee37f4c1855b46080f602e4de0c890cd
lahiruasiri <lahiruasiri1@gmail.com>
2025-05-03 01:35:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin:
       process.env.FRONTEND_URL ||
-      "http://localhost:5173https://countries-app-frontend.vercel.app",
+      "http://localhost:5173https://countries-app-frontend.vercel.app/",
     credentials: true,
   })
 );
